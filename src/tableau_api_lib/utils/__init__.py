722627965aeac0f62ce4080c19e178c7f65ac0f9
Elliott Stam <elliott.stam@gmail.com>
2019-10-02 16:52:38 -0700

    Added util functions to get a connection's users and user attributes

--- src/tableau_api_lib/utils/__init__.py
+++ src/tableau_api_lib/utils/__init__.py
@@ -1,3 +1,3 @@
 from .pagination import extract_pages
 from .schedules import clone_schedules, override_schedule_state, copy_schedule_state
-from .users import get_all_users
+from .users import get_all_users, get_all_user_emails, get_all_user_fullnames, get_all_user_names, get_all_user_roles
