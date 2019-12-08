023f5e50339f5644cbc5631eb922c7c496b016ac
Elliott Stam <elliott.stam@gmail.com>
2019-12-08 12:54:40 -0800

    added decorator verify_api_method_exists

--- src/tableau_api_lib/decorators/__init__.py
+++ src/tableau_api_lib/decorators/__init__.py
@@ -1,1 +1,2 @@
-from .verification import verify_response, verify_signed_in, verify_config_variables, verify_api_version
+from .verification import verify_response, verify_signed_in, verify_config_variables, verify_rest_api_version, \
+    verify_api_method_exists
--- src/tableau_api_lib/decorators/verification.py
+++ src/tableau_api_lib/decorators/verification.py
@@ -1,6 +1,7 @@
 import requests
 import warnings
 from functools import wraps
+from tableau_api_lib.exceptions import InvalidRestApiVersion
 
 
 def verify_response(success_code):
@@ -78,7 +79,7 @@ def verify_config_variables(func):
     return wrapper
 
 
-def verify_api_version(func):
+def verify_rest_api_version(func):
     """
     Verifies that the Tableau Server object is signed in.
     This decorates the function calls which send API api_requests to the server.
@@ -114,3 +115,23 @@ def verify_api_version(func):
         else:
             raise Exception('The Tableau Server connection is not logged in.')
     return wrapper
+
+
+def verify_api_method_exists(version_introduced):
+    """
+    Verifies that the connection's REST API version is equal to or greater than the version the method was introduced.
+
+    :param str version_introduced: the REST API version when the method was introduced
+    :return: decorated function
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            conn_api_version = self._config[self._env]['api_version']
+            if conn_api_version < version_introduced:
+                raise InvalidRestApiVersion(func,
+                                            api_version_used=conn_api_version,
+                                            api_version_required=version_introduced)
+            return func(self, *args, **kwargs)
+        return wrapper
+    return decorator
