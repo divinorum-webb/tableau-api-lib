540cce365fe44fc1d69f8f2f93310f101aa3a656
Elliott Stam <elliott.stam@gmail.com>
2019-12-11 07:56:01 -0800

    added functions to query the active site_name and content_url

--- src/tableau_api_lib/utils/querying/sites.py
+++ src/tableau_api_lib/utils/querying/sites.py
@@ -3,3 +3,11 @@ Functions to add:
 get_all_site_fields
 get_sites_dataframe
 """
+
+
+def get_active_site_name(conn):
+    return conn.query_site().json()['site']['name']
+
+
+def get_active_site_content_url(conn):
+    return conn.query_site().json()['site']['contentUrl']
