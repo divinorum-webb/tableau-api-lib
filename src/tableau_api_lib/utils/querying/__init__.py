75b1b46b8bd8b770810912e6d5e5753c43acf2bf
Elliott Stam <elliott.stam@gmail.com>
2019-12-11 07:54:56 -0800

    updated import statements

--- src/tableau_api_lib/utils/querying/__init__.py
+++ src/tableau_api_lib/utils/querying/__init__.py
@@ -1,3 +1,4 @@
 from .users import get_users_dataframe
 from .datasources import get_datasources_dataframe
 from .workbooks import get_workbooks_dataframe, get_views_dataframe
+from .sites import get_active_site_content_url, get_active_site_name
