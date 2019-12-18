9954f5fdf75ef7e4663211fe1c88a3b5d5376f20
Elliott Stam <elliott.stam@gmail.com>
2019-12-18 00:21:05 -0800

    added util functions for querying groups

--- src/tableau_api_lib/utils/querying/__init__.py
+++ src/tableau_api_lib/utils/querying/__init__.py
@@ -1,4 +1,5 @@
 from .users import get_users_dataframe
+from .groups import get_groups_dataframe, get_group_users_dataframe
 from .datasources import get_datasources_dataframe
 from .workbooks import get_workbooks_dataframe, get_views_dataframe
 from .sites import get_active_site_content_url, get_active_site_name
--- src/tableau_api_lib/utils/querying/groups.py
+++ src/tableau_api_lib/utils/querying/groups.py
@@ -1,5 +1,46 @@
 """
-Functions to add:
-get_all_group_fields
-get_groups_dataframe
+Helper functions for querying REST API data for groups
 """
+
+
+import pandas as pd
+from tableau_api_lib.utils import extract_pages
+from tableau_api_lib.utils.querying import get_users_dataframe
+
+
+def get_all_group_fields(conn):
+    all_groups = extract_pages(conn.query_groups, parameter_dict={'fields': 'fields=_all_'})
+    return all_groups
+
+
+def get_group_users(conn, group_id):
+    all_group_users = extract_pages(conn.get_users_in_group,
+                                    group_id=group_id,
+                                    parameter_dict={'fields': 'fields=_all_'})
+    return all_group_users
+
+
+def get_all_group_names(conn):
+    all_groups = get_all_group_fields(conn)
+    all_groupnames = [group['name'] for group in all_groups]
+    return all_groupnames
+
+
+def get_all_group_domain_names(conn):
+    all_groups = get_all_group_fields(conn)
+    all_group_roles = [group['domain']['name'] for group in all_groups]
+    return all_group_roles
+
+
+def get_groups_dataframe(conn):
+    groups_df = pd.DataFrame(get_all_group_fields(conn))
+    groups_df['domain'] = groups_df['domain'].apply(lambda x: x['name'])
+    return groups_df
+
+
+def get_group_users_dataframe(conn, group_id):
+    group_users_df = pd.DataFrame(get_group_users(conn, group_id))
+    users_df = get_users_dataframe(conn)
+    users_columns = list(users_df.columns)
+    all_group_users = group_users_df.merge(users_df, how='left', left_on='id', right_on='id', suffixes=('delete', None))
+    return all_group_users[users_columns]
