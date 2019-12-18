afd05ebcacd070d90daf3064ee0b2bb0cfec4dba
Elliott Stam <elliott.stam@gmail.com>
2019-12-18 00:24:55 -0800

    added imports for clone_users and clone_groups

--- src/tableau_api_lib/utils/cloning/__init__.py
+++ src/tableau_api_lib/utils/cloning/__init__.py
@@ -1,3 +1,5 @@
 from .projects import clone_projects
 from .sites import clone_sites
 from .schedules import clone_schedules, clone_schedule_state
+from .users import clone_users
+from .groups import clone_groups
