17103326c7b782d3e441a66f08043916e01e8cbe
Elliott Stam <elliott.stam@gmail.com>
2019-12-17 20:53:53 -0800

    added support for passing dynamic content id kwargs to extract_pages()

--- src/tableau_api_lib/utils/pagination.py
+++ src/tableau_api_lib/utils/pagination.py
@@ -19,11 +19,33 @@ def get_page_attributes(query):
         print("The query provided does not contain paged results.")
 
 
+def validate_kwargs(kwargs):
+    valid = True
+    valid_kwargs = [
+        'group_id',
+        'user_id',
+        'view_id',
+        'workbook_id',
+        'site_id'
+    ]
+    if len(kwargs.keys()) > 1:
+        valid = False
+    for kwarg in kwargs.keys():
+        if kwarg not in valid_kwargs:
+            valid = False
+    if not valid:
+        raise ValueError("""
+        {} is not a valid argument to the extract_pages function. Valid arguments are as follows: \n{}
+        """.format(list(kwargs.keys()).pop(), valid_kwargs))
+    return list(kwargs.keys())[0], list(kwargs.values())[0]
+
+
 def extract_pages(query_func,
                   starting_page=1,
                   page_size=100,
                   limit=None,
-                  parameter_dict={}):
+                  parameter_dict={},
+                  **kwargs):
     """
     :param query_func:          A callable function that will issue a GET request to Tableau Server.
     :type query_func:           function
@@ -37,6 +59,8 @@ def extract_pages(query_func,
     :type parameter_dict:       dict
     :return: extracted_pages    JSON or dict
     """
+    if kwargs:
+        arg_key, arg_value = validate_kwargs(kwargs)
     extracted_pages = []
     page_number = starting_page
     extracting = True
@@ -48,23 +72,29 @@ def extract_pages(query_func,
             'pageSize': 'pageSize={}'.format(page_size)
         }
         params.update(paginating_params)
-        query = query_func(parameter_dict=params).json()
+        if kwargs:
+            query = query_func(arg_value, parameter_dict=params).json()
+        else:
+            query = query_func(parameter_dict=params).json()
         page_number, page_size, total_available = get_page_attributes(query)
 
-        try:
-            outer_key = [key for key in query.keys() if key != 'pagination'].pop()
-            inner_key = list(query[outer_key].keys()).pop()
-            extracted_pages += query[outer_key][inner_key]
-        except IndexError:
-            raise ContentNotFound()
-
-        if limit:
-            if limit <= len(extracted_pages):
-                extracted_pages = extracted_pages[:limit]
-                extracting = False
-        elif total_available <= (page_number * page_size):
+        if total_available == 0:
             extracting = False
         else:
-            page_number += 1
+            try:
+                outer_key = [key for key in query.keys() if key != 'pagination'].pop()
+                inner_key = list(query[outer_key].keys()).pop()
+                extracted_pages += query[outer_key][inner_key]
+            except IndexError:
+                raise ContentNotFound()
+
+            if limit:
+                if limit <= len(extracted_pages):
+                    extracted_pages = extracted_pages[:limit]
+                    extracting = False
+            elif total_available <= (page_number * page_size):
+                extracting = False
+            else:
+                page_number += 1
 
     return extracted_pages
