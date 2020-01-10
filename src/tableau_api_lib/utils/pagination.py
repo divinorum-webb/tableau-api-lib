81b39c7875394c14f146ed027e7c38fe352757b5
Elliott Stam <elliott.stam@gmail.com>
2020-01-10 13:03:29 -0800

    improved and refactored handling of pagination and the extract_pages() method

--- src/tableau_api_lib/utils/pagination.py
+++ src/tableau_api_lib/utils/pagination.py
@@ -1,13 +1,12 @@
-from tableau_api_lib.exceptions import ContentNotFound
+from tableau_api_lib.exceptions import ContentNotFound, PaginationError
 
 
 def get_page_attributes(query):
     """
-    Get the page attributes (pageNumber, pageSize, totalAvailable) from a query and return their values.
+    Get page attributes (pageNumber, pageSize, totalAvailable) from a REST API paginated response.
 
-    :param query:   The results of the GET request query, containing paginated data.
-    :type query:    JSON or dict
-    :return:        page_number, page_size, total_available
+    :param dict query: the results of the GET request query, containing paginated data
+    :return: page_number, page_size, total_available
     """
     try:
         pagination = query['pagination']
@@ -16,85 +15,81 @@ def get_page_attributes(query):
         total_available = int(pagination['totalAvailable'])
         return page_number, page_size, total_available
     except KeyError:
-        print("The query provided does not contain paged results.")
-
-
-def validate_kwargs(kwargs):
-    valid = True
-    valid_kwargs = [
-        'group_id',
-        'user_id',
-        'view_id',
-        'workbook_id',
-        'site_id'
-    ]
-    if len(kwargs.keys()) > 1:
-        valid = False
-    for kwarg in kwargs.keys():
-        if kwarg not in valid_kwargs:
-            valid = False
-    if not valid:
-        raise ValueError("""
-        {} is not a valid argument to the extract_pages function. Valid arguments are as follows: \n{}
-        """.format(list(kwargs.keys()).pop(), valid_kwargs))
-    return list(kwargs.keys())[0], list(kwargs.values())[0]
+        raise PaginationError
 
 
 def extract_pages(query_func,
+                  content_id=None,
+                  *,
                   starting_page=1,
                   page_size=100,
                   limit=None,
-                  parameter_dict={},
-                  **kwargs):
+                  parameter_dict=None):
     """
-    :param query_func:          A callable function that will issue a GET request to Tableau Server.
-    :type query_func:           function
-    :param starting_page:       The page number to start on. Defaults to the first page (page_number = 1).
-    :type starting_page:        int
-    :param page_size:           The number of objects per page. If querying users, this is the number of users per page.
-    :type page_size:            int
-    :param limit:               The maximum number of objects to return. Default is no limit.
-    :type limit:                int
-    :param parameter_dict:      A dict whose values are appended to the REST API URL endpoint as URL parameters.
-    :type parameter_dict:       dict
-    :return: extracted_pages    JSON or dict
+    Extracts pages from paginated Tableau Server API responses.
+    :param function query_func: a callable function that will issue a GET request to Tableau Server
+    :param str content_id: the content ID value, if the query_func requires one [group_id, site_id, etc.]
+    :param int starting_page: the page number to start on. Defaults to the first page (page_number = 1)
+    :param int page_size: the number of objects per page. If querying users, this is the number of users per page
+    :param int limit: the maximum number of objects to return. Default is no limit
+    :param dict parameter_dict: a dict whose values are appended to the REST API URL endpoint as URL parameters
+    :return: JSON or dict
     """
-    if kwargs:
-        arg_key, arg_value = validate_kwargs(kwargs)
+    parameter_dict = parameter_dict or {}
     extracted_pages = []
     page_number = starting_page
     extracting = True
 
     while extracting:
-        params = parameter_dict.copy()
-        paginating_params = {
+        parameter_dict.update({
             'pageNumber': 'pageNumber={}'.format(page_number),
             'pageSize': 'pageSize={}'.format(page_size)
-        }
-        params.update(paginating_params)
-        if kwargs:
-            query = query_func(arg_value, parameter_dict=params).json()
-        else:
-            query = query_func(parameter_dict=params).json()
+        })
+        query = process_query(query_func, content_id, parameter_dict)
         page_number, page_size, total_available = get_page_attributes(query)
-
         if total_available == 0:
-            raise ValueError('The Tableau Server content being queried has at least one empty entity. Please Verify.')
-        else:
-            try:
-                outer_key = [key for key in query.keys() if key != 'pagination'].pop()
-                inner_key = list(query[outer_key].keys()).pop()
-                extracted_pages += query[outer_key][inner_key]
-            except IndexError:
-                raise ContentNotFound()
-
-            if limit:
-                if limit <= len(extracted_pages):
-                    extracted_pages = extracted_pages[:limit]
-                    extracting = False
-            elif total_available <= (page_number * page_size):
-                extracting = False
-            else:
-                page_number += 1
-
+            raise ContentNotFound()
+        extracted_pages, extracting, page_number = update_pagination_params(query,
+                                                                            extracted_pages,
+                                                                            page_number,
+                                                                            page_size,
+                                                                            total_available,
+                                                                            limit,
+                                                                            extracting)
     return extracted_pages
+
+
+def process_query(query_func, content_id, parameter_dict):
+    if content_id:
+        if isinstance(content_id, str):
+            try:
+                query = query_func(content_id, parameter_dict=parameter_dict).json()
+            except TypeError:
+                raise PaginationError(query_func)
+        else:
+            raise TypeError('The content ID value passed to {} must be a string.'.format(query_func.__name__))
+    else:
+        try:
+            query = query_func(parameter_dict=parameter_dict).json()
+        except TypeError:
+            raise PaginationError(query_func)
+    return query
+
+
+def update_pagination_params(query, extracted_pages, page_number, page_size, total_available, limit, extracting):
+    try:
+        outer_key = [key for key in query.keys() if key != 'pagination'].pop()
+        inner_key = list(query[outer_key].keys()).pop()
+        extracted_pages += query[outer_key][inner_key]
+    except IndexError:
+        raise ContentNotFound()
+
+    if limit:
+        if limit <= len(extracted_pages):
+            extracted_pages = extracted_pages[:limit]
+            extracting = False
+    elif total_available <= (page_number * page_size):
+        extracting = False
+    else:
+        page_number += 1
+    return extracted_pages, extracting, page_number
