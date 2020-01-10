d41dee40fd58955d2148cbd7bddbe13b311bfa0e
Elliott Stam <elliott.stam@gmail.com>
2020-01-10 13:30:27 -0800

    added get dataframe methods for: user favorites, schedules, sites

--- src/tableau_api_lib/utils/querying/favorites.py
+++ src/tableau_api_lib/utils/querying/favorites.py
@@ -2,3 +2,18 @@
 Functions to add:
 get_favorites_dataframe
 """
+
+
+import pandas as pd
+from tableau_api_lib.utils import extract_pages
+
+
+def get_all_favorite_fields(conn, user_id):
+    all_favorites = conn.get_favorites_for_user(user_id).json()['favorites']['favorite']
+    return all_favorites
+
+
+def get_user_favorites_dataframe(conn, user_id):
+    favorites_df = pd.DataFrame(get_all_favorite_fields(conn, user_id))
+    return favorites_df
+
--- src/tableau_api_lib/utils/querying/sites.py
+++ src/tableau_api_lib/utils/querying/sites.py
@@ -5,6 +5,20 @@ get_sites_dataframe
 """
 
 
+import pandas as pd
+from tableau_api_lib.utils import extract_pages
+
+
+def get_all_site_fields(conn):
+    all_sites = extract_pages(conn.query_sites, parameter_dict={'fields': 'fields=_all_'})
+    return all_sites
+
+
+def get_sites_dataframe(conn):
+    sites_df = pd.DataFrame(get_all_site_fields(conn))
+    return sites_df
+
+
 def get_active_site_name(conn):
     return conn.query_site().json()['site']['name']
 
