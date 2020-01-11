b493a536951f40a9d9b7eee245b7df62a38123b1
Elliott Stam <elliott.stam@gmail.com>
2020-01-11 15:14:42 -0800

    added docstrings for functions in datasources.py and favorites.py

--- src/tableau_api_lib/utils/querying/datasources.py
+++ src/tableau_api_lib/utils/querying/datasources.py
@@ -7,11 +7,21 @@ import pandas as pd
 from tableau_api_lib.utils import extract_pages
 
 
-def get_all_datasource_fields(conn):
+def get_all_datasource_fields(conn) -> list:
+    """
+    Queries all available datasource fields from Tableau Server.
+    :param class conn: the Tableau Server connection
+    :return: list
+    """
     all_datasources = extract_pages(conn.query_data_sources, parameter_dict={'fields': 'fields=_all_'})
     return all_datasources
 
 
-def get_datasources_dataframe(conn):
+def get_datasources_dataframe(conn) -> pd.DataFrame:
+    """
+    Returns a Pandas DataFrame of all available Tableau Server datasource fields.
+    :param class conn: the Tableau Server connection
+    :return: pd.DataFrame
+    """
     datasources_df = pd.DataFrame(get_all_datasource_fields(conn))
     return datasources_df
--- src/tableau_api_lib/utils/querying/favorites.py
+++ src/tableau_api_lib/utils/querying/favorites.py
@@ -8,12 +8,23 @@ import pandas as pd
 from tableau_api_lib.utils import extract_pages
 
 
-def get_all_favorite_fields(conn, user_id):
+def get_all_favorite_fields(conn, user_id) -> list:
+    """
+    Queries all available favorites for the specified user on Tableau Server.
+    :param class conn: the Tableau Server connection
+    :param str user_id: the Tableau Server user whose favorites will be queried
+    :return: list
+    """
     all_favorites = conn.get_favorites_for_user(user_id).json()['favorites']['favorite']
     return all_favorites
 
 
-def get_user_favorites_dataframe(conn, user_id):
+def get_user_favorites_dataframe(conn, user_id) -> pd.DataFrame:
+    """
+    Returns a Pandas DataFrame containing details for the specified user's favorites on Tableau Server.
+    :param class conn: the Tableau Server connection
+    :param str user_id: the Tableau Server user whose favorites will be queried
+    :return: pd.DataFrame
+    """
     favorites_df = pd.DataFrame(get_all_favorite_fields(conn, user_id))
     return favorites_df
-
