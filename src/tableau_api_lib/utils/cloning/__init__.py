4ae1d7ef58a9db6a97e59cb66d2d46d17752aa21
Elliott Stam <elliott.stam@gmail.com>
2020-02-04 12:00:14 -0800

    added support for cloning datasources (alpha)

--- src/tableau_api_lib/utils/cloning/__init__.py
+++ src/tableau_api_lib/utils/cloning/__init__.py
@@ -3,3 +3,4 @@ from .sites import clone_sites
 from .schedules import clone_schedules, clone_schedule_state
 from .users import clone_users
 from .groups import clone_groups
+from .datasources import clone_datasources
