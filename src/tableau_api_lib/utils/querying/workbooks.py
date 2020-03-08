e030e29bff199a9a5af5c012b3db86d91225c3a4
Elliott Stam <elliott.stam@gmail.com>
2020-03-08 14:49:49 -0700

    bug fix: modified ordering of get_embedded_datasources_dataframe() logic to avoid ValueError errors

--- src/tableau_api_lib/utils/querying/workbooks.py
+++ src/tableau_api_lib/utils/querying/workbooks.py
@@ -57,8 +57,8 @@ def get_embedded_datasources_dataframe(conn, workbooks_df, workbook_ids=None):
     :param list workbook_ids: a list of workbook IDs whose embedded datasources will be queried
     :return:
     """
-    workbook_ids = workbook_ids or []
     workbook_ids = workbook_ids.to_list() if isinstance(workbook_ids, pd.core.series.Series) else workbook_ids
+    workbook_ids = workbook_ids or []
     if any(workbook_ids):
         workbooks_df = workbooks_df[workbooks_df['id'].isin(workbook_ids)]
     embedded_datasources_df = pd.DataFrame()
