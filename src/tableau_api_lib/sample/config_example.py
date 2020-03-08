1965d71b68757d52cc5a28d405b6f144f9856922
Elliott Stam <elliott.stam@gmail.com>
2020-03-08 14:22:27 -0700

    updated the example configuration details to include personal access token setup

--- src/tableau_api_lib/sample/config_example.py
+++ src/tableau_api_lib/sample/config_example.py
@@ -4,9 +4,10 @@ sample_config = {
         'api_version': '<YOUR_API_VERSION>',
         'username': '<YOUR_USERNAME>',
         'password': '<YOUR_PASSWORD>',
+        # comment out username / password and uncomment the lines below to use personal access token authentication
+        # 'personal_access_token_name': '<YOUR_PERSONAL_ACCESS_TOKEN_NAME>',
+        # 'personal_access_token_secret': '<YOUR_PERSONAL_ACCESS_TOKEN_SECRET>',
         'site_name': '<YOUR_SITE_NAME>',
-        'site_url': '<YOUR_SITE_URL>',
-        'cache_buster': '<OPTIONAL_CACHE_BUSTER>',
-        'temp_dir': '/plugins/tableau/temp'
+        'site_url': '<YOUR_SITE_URL>'
     }
 }
