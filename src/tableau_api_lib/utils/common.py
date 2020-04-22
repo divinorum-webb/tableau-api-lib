92c990294e0117b63bf18a9ff94b78b97b005085
Elliott Stam <elliott.stam@gmail.com>
2020-04-22 09:16:00 -0700

    added docstring for util function flatten_dict_column()

--- src/tableau_api_lib/utils/common.py
+++ src/tableau_api_lib/utils/common.py
@@ -2,6 +2,13 @@ import urllib
 
 
 def flatten_dict_column(df, keys, col_name):
+    """
+    Extract the specified Pandas DataFrame dict column ('col_name'), creating new columns from the given keys.
+    :param pd.DataFrame df: the Pandas DataFrame whose dict column to be flattened
+    :param list keys: the keys of the dict column which will be extracted into new columns
+    :param str col_name: the name of the Pandas DataFrame column whose content will be flattened
+    :return: pd.DataFrame
+    """
     for key in keys:
         df[col_name + '_' + key] = df[col_name].apply(lambda col: col[key])
     df.drop(columns=[col_name], inplace=True)
