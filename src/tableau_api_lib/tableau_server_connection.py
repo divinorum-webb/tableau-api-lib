476f3c1927e7657f14fadc9f39d72ee81aade5a7
Elliott <elliott.stam@gmail.com>
2020-10-31 00:34:45 +0100

    FIX api version verification and query_sites params

--- src/tableau_api_lib/tableau_server_connection.py
+++ src/tableau_api_lib/tableau_server_connection.py
@@ -274,7 +274,7 @@ class TableauServerConnection:
         return response
 
     @verify_api_method_exists('2.3')
-    def query_site(self, include_usage_flag=False, parameter_dict=None):
+    def query_site(self, include_usage_flag=None, parameter_dict=None):
         """
         Queries details for the active site.
         :param bool include_usage_flag: True if including usage metrics, False otherwise
