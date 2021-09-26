664ebe6731826d2656fb68c79048582b3522b45e
elliottstam <elliott.stam@gmail.com>
2021-09-26 14:30:25 +0200

    added utils function `get_views_for_workbook_dataframe`

--- src/tableau_api_lib/utils/querying/__init__.py
+++ src/tableau_api_lib/utils/querying/__init__.py
@@ -2,7 +2,7 @@ from .users import get_users_dataframe
 from .groups import get_groups_dataframe, get_group_users_dataframe, get_groups_for_a_user_dataframe
 from .datasources import get_datasources_dataframe, get_datasource_connections_dataframe
 from .workbooks import get_workbooks_dataframe, get_views_dataframe, get_workbook_connections_dataframe, \
-    get_embedded_datasources_dataframe, get_view_data_dataframe
+    get_embedded_datasources_dataframe, get_view_data_dataframe, get_views_for_workbook_dataframe
 from .sites import get_sites_dataframe, get_active_site_content_url, get_active_site_name, get_active_site_id
 from .projects import get_projects_dataframe
 from .schedules import get_schedules_dataframe
