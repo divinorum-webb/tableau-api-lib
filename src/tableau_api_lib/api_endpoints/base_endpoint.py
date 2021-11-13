3c30c078e4fa58616f6791b625a9795cda219b0b
Subhash-Daggubati <daggubatisubhash@gmail.com>
2021-11-12 21:35:52 -0500

    fix for parameter_dict issue #54

--- src/tableau_api_lib/api_endpoints/base_endpoint.py
+++ src/tableau_api_lib/api_endpoints/base_endpoint.py
@@ -34,7 +34,8 @@ class BaseEndpoint:
         text_to_append = "?" if any(self._params_exist) else ""
         for i, text in enumerate(self._params_text):
             if self._params_exist[i]:
-                text_to_append += text if text_to_append.endswith('?') else ('&' + text)
+                parameter_text = self._params_exist[i]+'='+text
+                text_to_append += parameter_text if text_to_append.endswith('?') else ('&' + parameter_text)
         return "{0}{1}".format(url, text_to_append)
     
     def _invalid_parameter_exception(self):
