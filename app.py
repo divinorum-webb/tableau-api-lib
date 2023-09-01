d7d96f933382ebef909a02ce31f3e8c918d01504
Patrick K <pati93@gmx.de>
2023-09-01 13:41:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ async def execute_function():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
